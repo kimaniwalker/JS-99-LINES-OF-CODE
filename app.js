7eb1acf7dd4168fc87a2697d6d37d71bfef26b23
Kimani Walker <kimaniwalker@gmail.com>
2018-06-06 12:25:34 -0500

    Fixed loop parameters

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 console.log('Hello World');
 
 var friends = ["Prep","Kiddo","Sammie","Kaden","Caleb"];
+
+//Test
 var lyrics = ("song lyrics");
 
 // (var i) initilization statement - initilizes variable to maintain loop
@@ -9,15 +11,16 @@ var lyrics = ("song lyrics");
 // put in curly braces which is block of code that will run
 for(var f = 0; f < friends.length; f++ ){
     console.log(friends[f]);
-    for(var i = 0; i < 100; i++ ){
+    for(var i = 99; i >= 1; i-- ){
         console.log(i + ' song lyrics');
     }
 }
 
-for(var i = 0; i < 100; i++ ){
+for(var i = 99; i >= 1; i-- ){
     console.log(i);
 }
 
+//Test
 for(var s = 0; s < lyrics.length; s++ ){
     console.log(lyrics[s]);
 }
