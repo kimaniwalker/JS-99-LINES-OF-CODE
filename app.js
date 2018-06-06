fe9753b21e326ee7eb3b0e12e28e0d571f707c47
Kimani Walker <kimaniwalker@gmail.com>
2018-06-06 10:30:54 -0500

    Added 2 Loops

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 console.log('Hello World');
 
-var friend = ["Prep","Kiddo","Sammie","Kaden","Caleb"];
+var friends = ["Prep","Kiddo","Sammie","Kaden","Caleb"];
 
 
+// (var i) initilization statement - initilizes variable to maintain loop
+// conditional statement on whether loop should continue to run
+// What we should do after loop has run/ increment variable
+// put in curly braces which is block of code that will run
+for(var f = 0; f < friends.length; f++ ){
+    console.log(friends[f]);
+}
+
+for(var i = 0; i < 100; i++ ){
+    console.log(i);
+}
